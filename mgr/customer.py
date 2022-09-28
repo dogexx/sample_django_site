3bd6c47a2536b7a1aadafdb441f5639ff2d182d9
xiangxu <xxi17697@gmail.com>
2022-09-28 13:20:16 +0800

    fix

--- mgr/customer.py
+++ mgr/customer.py
@@ -4,9 +4,10 @@ from common.models import Customer
 
 
 def dispatcher(request):
-    # 将请求参数统一放入request 的 params 属性中，方便后续处理
-
-    # 根据session判断用户是否是登录的管理员用户
+    """
+    将请求参数统一放入request 的 params 属性中，方便后续处理
+    根据session判断用户是否是登录的管理员用户
+    """
     if 'usertype' not in request.session:
         return JsonResponse({
             'ret': 302,
@@ -20,6 +21,7 @@ def dispatcher(request):
             'msg': '用户非mgr类型',
             'redirect': '/mgr/sign.html'} ,
             status=302)
+
     # GET请求 参数在url中，同过request 对象的 GET属性获取
     if request.method == 'GET':
         request.params = request.GET
@@ -28,7 +30,6 @@ def dispatcher(request):
     elif request.method in ['POST','PUT','DELETE']:
         # 根据接口，POST/PUT/DELETE 请求的消息体都是 json格式
         request.params = json.loads(request.body)
-
 
     # 根据不同的action分派给不同的函数进行处理
     action = request.params['action']
@@ -40,14 +41,13 @@ def dispatcher(request):
         return modifycustomer(request)
     elif action == 'del_customer':
         return deletecustomer(request)
-
     else:
         return JsonResponse({'ret': 1, 'msg': '不支持该类型http请求'})
 
 def listcustomers(request):
+    """列出所有customer"""
     # 返回一个 QuerySet 对象 ，包含所有的表记录
     qs = Customer.objects.values()
-
     # 将 QuerySet 对象 转化为 list 类型
     # 否则不能 被 转化为 JSON 字符串
     retlist = list(qs)
@@ -55,27 +55,18 @@ def listcustomers(request):
     return JsonResponse({'ret': 0, 'retlist': retlist})
 
 def addcustomer(request):
-
-    info    = request.params['data']
-
-    # 从请求消息中 获取要添加客户的信息
-    # 并且插入到数据库中
-    # 返回值 就是对应插入记录的对象 
+    """新增customer"""
+    info = request.params['data']
     record = Customer.objects.create(name=info['name'] ,
                             phonenumber=info['phonenumber'] ,
                             address=info['address'])
 
-
     return JsonResponse({'ret': 0, 'id':record.id})
 
 def modifycustomer(request):
-
-    # 从请求消息中 获取修改客户的信息
-    # 找到该客户，并且进行修改操作
-    
+    """从请求消息中 获取修改客户的信息"""
     customerid = request.params['id']
     newdata    = request.params['newdata']
-
     try:
         # 根据 id 从数据库中找到相应的客户记录
         customer = Customer.objects.get(id=customerid)
@@ -84,8 +75,6 @@ def modifycustomer(request):
                 'ret': 1,
                 'msg': f'id 为`{customerid}`的客户不存在'
         }
-
-
     if 'name' in  newdata:
         customer.name = newdata['name']
     if 'phonenumber' in  newdata:
@@ -99,19 +88,15 @@ def modifycustomer(request):
     return JsonResponse({'ret': 0})
 
 def deletecustomer(request):
-
+    """根据 id 从数据库中删除相应的客户记录"""
     customerid = request.params['id']
-
     try:
-        # 根据 id 从数据库中找到相应的客户记录
         customer = Customer.objects.get(id=customerid)
     except Customer.DoesNotExist:
         return  {
                 'ret': 1,
                 'msg': f'id 为`{customerid}`的客户不存在'
         }
-
-    # delete 方法就将该记录从数据库中删除了
     customer.delete()
 
     return JsonResponse({'ret': 0})
